68bac376806fa25bafdd0efe9f81a80ba285dcb8
Jaime Lopez <jaimelopezmislata@gmail.com>
2025-03-11 10:58:15 +0100

    Ejercicios resueltos

--- scripts.js
+++ scripts.js
@@ -1,1 +1,32 @@
+// EJERCICIO 1
 console.log(document.title);
+
+// EJERCICIO 2
+const titleGen1 = document.getElementById('gen-1');
+titleGen1.innerText = "Generasión 1 Pokimon";
+
+//EJERCICIO 3
+const gen1Box = document.querySelector('.infocard-list');
+gen1Box.style.backgroundColor = 'pink';
+
+//EJERCICIO 4
+console.log(document.URL);
+console.log(document.domain);
+
+//EJERCICIO 5
+const imagesPokemon = document.querySelectorAll('.img-fixed.img-sprite');
+console.log(imagesPokemon);
+
+//EJERCICIO 6
+imagesPokemon.forEach((imgPokemon) => {
+    imgPokemon.setAttribute('src', 'https://media.giphy.com/media/2v170e71aanfi/giphy.gif');
+}) 
+
+//EJERCICIO 7
+const pokemones = document.querySelectorAll('.infocard');
+
+pokemones.forEach((flyPokemon) => {
+    if(flyPokemon.innerHTML.includes('flying')) {
+        flyPokemon.style.backgroundColor = 'lightgrey';
+    }   
+})
